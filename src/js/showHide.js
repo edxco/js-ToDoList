ec7d7c447a8aa2a8a8a5c614c11b38c2a1453f4e
Kingsley <kingobaino@gmail.com>
2021-03-02 00:57:23 +0100

    Refactor taskAddForm.js file

--- src/js/showHide.js
+++ src/js/showHide.js
@@ -1,9 +1,9 @@
 const showHide = (div) => {
-    if (div.style.display === 'none') {
-        div.style.display = 'block';
-    } else {
-        div.style.display = 'none';
-    }
-}
+  if (div.style.display === 'none') {
+    div.style.display = 'block';
+  } else {
+      div.style.display = 'none';
+  }
+};
 
 export default showHide;
