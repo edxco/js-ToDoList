eaef78fbd5d089c8fe5c0eb7cc98a17638f53b0a
Kingsley <kingobaino@gmail.com>
2021-03-12 20:24:47 +0100

    Write test for the projectAddBtn function

--- src/js/projectAddBtn.js
+++ src/js/projectAddBtn.js
@@ -1,6 +1,3 @@
-import down from '../img/chevron-down.png';
-import plus from '../img/plus.png';
-
 const projectAddBtn = () => {
   const projectAddBtnDiv = document.createElement('div');
   projectAddBtnDiv.className = 'antialiased w-full';
@@ -10,13 +7,13 @@ const projectAddBtn = () => {
   const divDown = document.createElement('div');
   const imageDown = document.createElement('img');
   imageDown.className = 'w-3 ml-4 mr-7';
-  imageDown.src = down;
+  imageDown.src = './media/chevron-down.png';
   const divTitle = document.createElement('div');
   divTitle.className = 'w-full text-center text-gray-700';
   divTitle.textContent = 'Projects List';
   const imagePlus = document.createElement('img');
   imagePlus.className = 'mx-5';
-  imagePlus.src = plus;
+  imagePlus.src = './media/plus.png';
   projectAddBtnDiv.append(btnAddProj);
   btnAddProj.append(divDown, divTitle, imagePlus);
   divDown.append(imageDown);
